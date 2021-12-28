89c62a4e6a6922bd6eafc9ccdf27d9dffebe7ab3
Venus Teah <venus.teah@gmail.com>
2021-12-28 16:52:02 -0500

    Create functions based on User Story

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 console.log("JS is connected")
+// As a user, I should be able to start a new tic tac toe game
+function startGame()
+
+
+
+// As a user, I should be able to click on a square to add X first and then O, and so on
+function clickSquare()
+
+
+// As a user, I should be shown a message after each turn for if I win, lose, tie or who's turn it is next
+function displayYouWin()
+function displayYouLose()
+function displayDraw()
+function currrentPlayer()
+
+
+// As a user, I should not be able to click the same square twice
+function checkSquare()
+
+
+// As a user, I should be shown a message when I win, lose or tie
+
+
+// As a user, I should not be able to continue playing once I win, lose, or tie
+function stopGame()
+
+// As a user, I should be able to play the game again without refreshing the page
