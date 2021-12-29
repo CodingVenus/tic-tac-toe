38f2325b04edbaca0cf6dbd21d4925ee25138eb3
Venus Teah <venus.teah@gmail.com>
2021-12-28 20:19:25 -0500

    add query selector

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
-console.log("JS is connected")
-// As a user, I should be able to start a new tic tac toe game
-function startGame()
+// console.log("JS is connected")
+// // As a user, I should be able to start a new tic tac toe game
+// function startGame()
 
 
 
-// As a user, I should be able to click on a square to add X first and then O, and so on
-function clickSquare()
+// // As a user, I should be able to click on a square to add X first and then O, and so on
+// function clickSquare()
+// let square = document.querySelector('.square')
+// console.log(square)
 
 
-// As a user, I should be shown a message after each turn for if I win, lose, tie or who's turn it is next
-function displayYouWin()
-function displayYouLose()
-function displayDraw()
-function currrentPlayer()
+// // As a user, I should be shown a message after each turn for if I win, lose, tie or who's turn it is next
+// function displayWinner()
+// function displayDraw()
+// function currrentPlayer()
 
 
-// As a user, I should not be able to click the same square twice
-function checkSquare()
+// // As a user, I should not be able to click the same square twice
+// function checkSquare()
 
 
-// As a user, I should be shown a message when I win, lose or tie
+// // As a user, I should be shown a message when I win, lose or tie
 
 
-// As a user, I should not be able to continue playing once I win, lose, or tie
-function stopGame()
+// // As a user, I should not be able to continue playing once I win, lose, or tie
+// function stopGame()
 
-// As a user, I should be able to play the game again without refreshing the page
+// // As a user, I should be able to play the game again without refreshing the page
+// //preventdeafult?
+
+
+//VARIABLES
+let currentPlayer;
+
+//QuerySelectors
+document.querySelector('.square').addEventListener('click', )
