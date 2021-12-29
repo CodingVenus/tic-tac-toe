fe35c19fd65e185bef22e030530ce87bff684e00
Venus Teah <venus.teah@gmail.com>
2021-12-29 00:01:55 -0500

    added combination matrix for winning

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 // console.log("JS is connected")
+//VARIABLES
+let currentPlayer = "";
+let gameBoard = "";
+
+const winningCombinations = [
+    //Across
+    [0, 1, 2]
+    [3, 4, 5]
+    [6, 7, 8]
+    //Down
+    [0, 3, 6]
+    [1, 4, 7]
+    [2, 5, 8]
+    //Diagonal
+    [1, 4, 8]
+    [6, 4, 2]
+
+]
+
+
+
+
 // // As a user, I should be able to start a new tic tac toe game
 // function startGame()
+
 
 
 
@@ -11,10 +34,8 @@ function clickSquare(square, index) {
     let squareClickedIndex = index;
     console.log(squareClickedIndex)
     //??? how to get index of square clicked
-
 }
-// let square = document.querySelector('.square')
-// console.log(square)
+
 
 
 // // As a user, I should be shown a message after each turn for if I win, lose, tie or who's turn it is next
@@ -37,8 +58,6 @@ function clickSquare(square, index) {
 // //preventdeafult?
 
 
-// //VARIABLES
-// let currentPlayer;
 
 
 
